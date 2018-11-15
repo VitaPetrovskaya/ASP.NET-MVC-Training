2530b6e08a497c0dcd0a7292e062fcf64b57430e
VitaPetrovskaya <vita.petrovskaya@gmail.com>
2018-11-15 13:49:22 +0300

    Add Fibonacci sequence

---
+++ NET.W.2018.Petrovskaya.12/FibonacciSequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSequence
+{
+     public class Program
+     {
+          /// <summary>
+          /// Test FibonacciSequence class.
+          /// </summary>
+          /// <param name="args"></param>
+          public static void Main(string[] args)
+          {
+               FibonacciSequence fibonacci = new FibonacciSequence(10);
+               foreach (int num in fibonacci)
+               {
+                    Console.Write(num + " ");
+               }
+
+               Console.WriteLine();
+               fibonacci.GenerateNewSequence(20);
+               foreach (int num in fibonacci)
+               {
+                    Console.Write(num + " ");
+               }
+
+               Console.ReadLine();
+          }
+     }
+}
